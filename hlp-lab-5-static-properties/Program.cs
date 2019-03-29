d95a12bc7cb6af179a00ec2675ef16a5f8728114
killbond <killbond@mail.ru>
2019-03-30 00:15:39 +0300

    initial commit

---
+++ hlp-lab-5-static-properties/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hlp_lab_5_static_properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test t1 = new Test();
+            Test t2 = new Test();
+            Test t3 = new Test();
+
+            System.Console.Out.WriteLine("{0}, {1}, {2}", t1.GetCount(), t2.GetCount(), t3.GetCount());
+            System.Console.Out.WriteLine("{0}, {1}, {2}", t1.GetIndex(), t2.GetIndex(), t3.GetIndex());
+
+            System.Console.In.Read();
+        }
+    }
+}
---
+++ hlp-lab-5-static-properties/Test.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hlp_lab_5_static_properties
+{
+    class Test
+    {
+        private static int count = 0;
+
+        private int index;
+
+        public Test()
+        {
+            count++;
+            this.index = count;
+        }
+
+        public int GetCount()
+        {
+            return count;
+        }
+
+        public int GetIndex()
+        {
+            return this.index;
+        }
+    }
+}
